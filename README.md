c2ceeec45d76742276ddadc5cd65944dafc5b07f
Kai Hatje <kai.hatje@app-akademie.com>
2024-04-04 12:07:53 +0200

    Run on iOS, refactoring of Widget functions, add content to README.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # maps_workshop_example
 
-A new Flutter project.
+Ein kleines Projekt, das zeigt, wie man Google Maps Alternativen verwendet.
+Erlaubt das Anzeigen des eigenen Standorts, das Setzen von Markern und die Berechnung einer Route vom eigenen Standort zu den Markern.
+
+## Setup
+
+1. Das Projekt clonen.
+2. `flutter pub get` ausführen.
+3. Ein Firebase-Projekt erstellen.
+4. Eine Firestore-Datenbank erstellen.
+5. `flutterfire configure` ausführen.
+6. App starten.
--- lib/features/map/presentation/map_screen.dart
+++ lib/features/map/presentation/map_screen.dart
@@ -13,21 +13,49 @@ class MapScreen extends StatelessWidget {
     return FutureBuilder(
       future: mapNotifier.initControllerFuture,
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return Stack(children: [
-            buildOSMFlutter(mapNotifier),
-            buildControlButtons(mapNotifier),
-          ]);
-        } else {
-          return const CircularProgressIndicator();
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return const Text("wtf???");
+          case ConnectionState.waiting:
+            return const CircularProgressIndicator();
+          // Should never fire, as we are using a Future, not a stream.
+          case ConnectionState.active:
+            return const Text("Should never be visible");
+          case ConnectionState.done:
+            if (snapshot.hasError) {
+              return Center(
+                child: Column(
+                  children: [
+                    const Text("An Error has occurred!"),
+                    Text(snapshot.error.toString()),
+                  ],
+                ),
+              );
+            } else if (snapshot.hasData) {
+              return const Text(
+                  "Should not happen, our snapshot doesn't have data.");
+            } else {
+              return const Stack(children: [
+                MapsExampleOSMFlutter(),
+                MapsExampleControlButtons(),
+              ]);
+            }
         }
       },
     );
   }
+}
 
-  OSMFlutter buildOSMFlutter(MapNotifier mapNotifier) {
+class MapsExampleOSMFlutter extends StatelessWidget {
+  const MapsExampleOSMFlutter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final MapNotifier mapNotifier = Provider.of<MapNotifier>(context);
+
     return OSMFlutter(
-      onGeoPointClicked: (p0) => mapNotifier.gernerRouteFromCurrentLocation(p0),
+      onGeoPointClicked: (p0) =>
+          mapNotifier.generateRouteFromCurrentLocation(p0),
       // onLocationChanged: (p0) => mapNotifier.updateCurrentLocation(p0),
       controller: mapNotifier.controller,
       osmOption: OSMOption(
@@ -72,8 +100,15 @@ class MapScreen extends StatelessWidget {
       ),
     );
   }
+}
 
-  Widget buildControlButtons(MapNotifier mapNotifier) {
+class MapsExampleControlButtons extends StatelessWidget {
+  const MapsExampleControlButtons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final MapNotifier mapNotifier = Provider.of<MapNotifier>(context);
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
@@ -82,46 +117,52 @@ class MapScreen extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              buildIconButton(
-                onPressed: () async {
-                  await mapNotifier.startPositionPicker();
-                },
-                iconData: Icons.add_location,
-                color: const Color.fromARGB(255, 30, 101, 35),
-              ),
-              buildIconButton(
-                onPressed: () {
-                  mapNotifier.addDestinationMarker();
-                },
-                iconData: Icons.location_on,
-                color: const Color.fromARGB(255, 86, 76, 175),
-              ),
-              buildIconButton(
-                onPressed: () {
-                  mapNotifier.addCurrentLocationMarkerAndZoom();
-                },
-                iconData: Icons.search,
-                color: const Color.fromARGB(255, 244, 162, 54),
-              ),
-              buildIconButton(
-                onPressed: () {
-                  mapNotifier.deleteAllLocationsInFirebase();
-                },
-                iconData: Icons.restore,
-                color: const Color.fromARGB(255, 244, 79, 54),
-              ),
+              MapsExampleIconButton(
+                  onPressed: () async {
+                    await mapNotifier.startPositionPicker();
+                  },
+                  iconData: Icons.add_location,
+                  color: const Color.fromARGB(255, 30, 101, 35)),
+              MapsExampleIconButton(
+                  onPressed: () {
+                    mapNotifier.addDestinationMarker();
+                  },
+                  iconData: Icons.location_on,
+                  color: const Color.fromARGB(255, 86, 76, 175)),
+              MapsExampleIconButton(
+                  onPressed: () {
+                    mapNotifier.addCurrentLocationMarkerAndZoom();
+                  },
+                  iconData: Icons.search,
+                  color: const Color.fromARGB(255, 244, 162, 54)),
+              MapsExampleIconButton(
+                  onPressed: () {
+                    mapNotifier.deleteAllLocationsInFirebase();
+                  },
+                  iconData: Icons.restore,
+                  color: const Color.fromARGB(255, 244, 79, 54)),
             ],
           ),
         ),
       ],
     );
   }
+}
 
-  IconButton buildIconButton({
-    required VoidCallback onPressed,
-    required IconData iconData,
-    required Color color,
-  }) {
+class MapsExampleIconButton extends StatelessWidget {
+  const MapsExampleIconButton({
+    super.key,
+    required this.onPressed,
+    required this.iconData,
+    required this.color,
+  });
+
+  final VoidCallback onPressed;
+  final IconData iconData;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
     return IconButton(
       onPressed: onPressed,
       icon: Icon(
